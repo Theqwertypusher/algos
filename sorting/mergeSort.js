f25ae8d0626e5149fabd6c4731e56ad103eab220
JVictor <qwerty920727@gmail.com>
2023-09-11 12:34:56 -0400

    added mergeSort

---
+++ sorting/mergeSort.js
@@ -0,0 +1,40 @@
+/**
+Helper function for MergeSort
+*/
+const merge = (arr1, arr2) => {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+};
+
+/**
+Sorting algorithm that splits input array down to smallest subarray 
+which is automatically sorted then merges back into original arrays
+*/
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
+
+module.exports = { mergeSort };
