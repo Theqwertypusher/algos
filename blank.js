5c62627c9936ce4ef81cc139e98e6541967dfd33
JVictor <qwerty920727@gmail.com>
2023-09-15 18:31:20 -0400

    added blank file to write out algos

---
+++ blank.js
@@ -0,0 +1,16 @@
+/* 
+
+one sentence descriptions of algorithms and patterns
+bubble, selection nested loops to check for highest value or lowest value
+insertion sorts - iterates array and sorts the array left of the iterator
+merge sort - uses helper function to merge; recursion to split 
+quick sort - uses helper pivot function initially based on last element. 
+Swaps all numbers less and returns index which is the index that is sorted. 
+Quick sort. Recursively calls pivot before and after the initial pivot index. And returns arr
+*/
+
+const func = (arr) => {
+  return arr;
+};
+
+console.log(func([2, 1, 3, 4, 5, 2]));
